580cea0dc6d2da9ea7534b86b295a09d4a524ee4
Eric McKinney <mckinney99@gmail.com>
2017-11-06 19:51:46 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Songs 
+  attr_accessor :songs,
+
+  def initialize
+    @songs = []
+    @songs
+  end
+
+  def add_song(song) 
+    @songs = songs.each do |song|
+      self.artist.name
+  end
+      
+  def add_song_by_name(song)
+
+  end  
