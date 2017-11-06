94eb89fadae03203311b7f4ab2eed41ae98dc6d3
Eric McKinney <mckinney99@gmail.com>
2017-11-06 20:52:01 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post
+  attr_accessor :name, :author
+
+  def initialize(name)
+    @name = name
+    @author = author
+  end
+
+  ded author_name
+  @author ? self.author.name : nil
+end
+end
