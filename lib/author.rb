f38326a592dd98f5c3e578ee5a8e299b66f9bbe8
Eric McKinney <mckinney99@gmail.com>
2017-11-06 20:50:47 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+  attr_accessor :name, :posts
+
+  @@post_count = 1
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+  end
+
+  def add_post_by_title(post)
+    post = Post.new(name)
+    @posts << post
+    post.author = self
+  end
+
+  def self.post_count
+    @@post_count
+  end
+  
+  
+
+  
