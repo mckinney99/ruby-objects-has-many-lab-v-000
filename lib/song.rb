676afefa28970e39ff752ce94bf1240f85fc73a4
Eric McKinney <mckinney99@gmail.com>
2017-11-06 19:55:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,15 @@ class Songs
   attr_accessor :name
 
   def initialize(name)
-    @name = name 
+    @name = name
   end
 
   def add_song(song)
     @songs = songs.each do |song|
-      self.artist.name
+      @songs << self.artist.name
   end
 
 
   def add_song_by_name(song)
-  
+
   end
